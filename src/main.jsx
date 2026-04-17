c8b079ff6e4b45aba6fae86b6ffa957194ddfc1c
Endan Suprihno <endansup@gmail.com>
2026-04-17 08:13:46 +0700

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import Fermionisme from "./Fermionisme.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Fermionisme />
-  </StrictMode>
-);
